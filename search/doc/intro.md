9d0b702fabe3ee302601af83ad0dfe3a382fd2ca
Cody Canning <cocanning11@gmail.com>
2013-07-09 18:49:18 -0400

    added binary search and dfs

---
+++ search/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to search
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ search/test/search/core_test.clj
@@ -0,0 +1,7 @@
+(ns search.core-test
+  (:use clojure.test
+        search.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
